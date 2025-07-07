c4d01d3108ca98db90e577b87b7d727e88eb0249
waans14 <afwan.sutdrajat@gmail.com>
2025-07-07 10:52:44 +0800

    mengatur route & supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,3 @@
-// src/supabaseClient.js
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://pygxypljmxbausdhmbrr.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5Z3h5cGxqbXhiYXVzZGhtYnJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4MTEzNjEsImV4cCI6MjA2NzM4NzM2MX0.RLdehxwpyjZ4SUJohHaCxbBqNspZ8vEIHfZPzEE7MrE";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
